6b1da359881eccadecc8b583e07a84fa95d5c850
심원진 <simwj0227@gmail.com>
2024-06-11 12:13:20 +0900

    External Js file

--- js/app.js
+++ js/app.js
@@ -1,16 +1,14 @@
-// script.js
-document.addEventListener('DOMContentLoaded', function() {
-  // Scroll to top functionality
-  document.getElementById('scrollToTopButton').addEventListener('click', function() {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    });
+document.getElementById("scrollToTopBtn").addEventListener("click", function() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
   });
-
-  // Load Font Awesome dynamically
-  var script = document.createElement('script');
-  script.src = "https://kit.fontawesome.com/2aca29b317.js";
-  script.crossOrigin = "anonymous";
-  document.head.appendChild(script);
 });
+
+
+
+
+
+
+
+

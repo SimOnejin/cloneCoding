22d13c8852adadb2f102d69134876b866efa0c4e
SWIISSS <david217981@gmail.com>
2024-06-11 16:16:45 +0900

    logo

--- js/navbar.js
+++ js/navbar.js
@@ -1,17 +1,24 @@
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("intro");
-var span = document.getElementsByClassName("close")[0];
+// About navbar
+document.addEventListener("DOMContentLoaded", function() {
+  fetch('navbar.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('navbar').innerHTML = data;
+      var modal = document.getElementById("myModal");
+      var btn = document.getElementById("intro");
+      var span = document.getElementsByClassName("close")[0];
+      btn.onclick = function() {
+        modal.style.display = "block";
+      }
 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+      span.onclick = function() {
+        modal.style.display = "none";
+      }
 
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+      window.onclick = function(event) {
+        if (event.target == modal) {
+          modal.style.display = "none";
+        }
+      }
+    });
+});

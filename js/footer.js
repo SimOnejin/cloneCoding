32b5cac1c19c7792fcc67b3c898b1f00b2e1d0bb
심원진 <simwj0227@gmail.com>
2024-06-11 14:24:23 +0900

    Top up Button

--- js/footer.js
+++ js/footer.js
@@ -1,8 +1,41 @@
-// About footer
 document.addEventListener("DOMContentLoaded", function() {
   fetch('footer.html')
     .then(response => response.text())
     .then(data => {
       document.getElementById('footer').innerHTML = data;
+
+      // Scroll to top functionality
+      var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+      var rootElement = document.documentElement;
+
+      // function handleScroll() {
+      //   // Show button when scrolled down 100px from top
+      //   if (rootElement.scrollTop > 100) {
+      //     scrollToTopBtn.style.display = "block";
+      //   } else {
+      //     scrollToTopBtn.style.display = "none";
+      //   }
+      // }
+
+      function scrollToTop() {
+        // Scroll to top behavior
+        rootElement.scrollTo({
+          top: 0,
+          behavior: "smooth"
+        });
+
+        // Temporarily disable handleScroll to prevent hiding the button
+        window.removeEventListener("scroll", handleScroll);
+        scrollToTopBtn.style.display = "block";  // Ensure button remains visible
+        setTimeout(function() {
+          window.addEventListener("scroll", handleScroll);
+        }, 1000);  // Re-enable handleScroll after scrolling completes
+      }
+
+      scrollToTopBtn.addEventListener("click", scrollToTop);
+      document.addEventListener("scroll", handleScroll);
+
+      // Initial call to handleScroll to set button state on page load
+      handleScroll();
     });
 });

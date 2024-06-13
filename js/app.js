644a59f01a8f1ceadb9427ef5466fbf0516e9a05
심원진 <simwj0227@gmail.com>
2024-06-13 10:11:25 +0900

    Exchange Index to Test

--- js/app.js
+++ js/app.js
@@ -16,3 +16,24 @@ document.addEventListener("DOMContentLoaded", function() {
 window.addEventListener('load', () => {
   setTimeout(()=>scrollTo(0,0),100);
 })
+
+
+
+//Slide Effect
+document.addEventListener("DOMContentLoaded", function() {
+    let observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('animated');
+            } else {
+                entry.target.classList.remove('animated');
+            }
+        });
+    }, { threshold: 0.5 });
+
+    let slides = document.querySelectorAll('.slide');
+    slides.forEach(box => observer.observe(slide));
+});
+
+
+
